d06354b835cd929e95943a45b6c2cdbeb904cdf5
Andre Purnomo <andre.purnomo@propellerhead.co.nz>
2022-02-20 19:57:25 +0700

    Develop ASP.NET Web App

---
+++ CE.Assessment/CE.Assessment.Web/Models/OrderDetailModel.cs
@@ -0,0 +1,23 @@
+﻿namespace CE.Assessment.Web.Models
+{
+    public class OrderDetailModel
+    {
+        public int Id { get; set; }
+        public string ChannelName { get; set; }
+        public string GlobalChannelName { get; set; }
+        public string ChannelOrderNo { get; set; }
+        public string MerchantOrderNo { get; set; }
+        public string Status { get; set; }
+        public string SubTotalInclVat { get; set; }
+        public string SubTotalVat { get; set; }
+        public string ShippingCostsVat { get; set; }
+        public string TotalInclVat { get; set; }
+        public string TotalVat { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string PaymentMethod { get; set; }
+        public string ShippingCostsInclVat { get; set; }
+        public string CurrencyCode { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
