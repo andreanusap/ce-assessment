40b3569fbd241c888fdeb94c399f5c2a60d8b44c
Andre Purnomo <andre.purnomo@propellerhead.co.nz>
2022-02-25 18:44:41 +0700

    Implement Web Client API

---
+++ CE.Assessment/CE.Assessment.WebApi/Controllers/OrderController.cs
@@ -0,0 +1,63 @@
+﻿using CE.Assessment.BusinessLogic.Entities;
+using CE.Assessment.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CE.Assessment.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class OrderController : ControllerBase
+    {
+        private readonly ILogger<OrderController> _logger;
+        private readonly IOrderService _orderService;
+
+        public OrderController(ILogger<OrderController> logger, IOrderService orderService)
+        {
+            _logger = logger;
+            _orderService = orderService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<OrderResponse>> GetOrders([FromQuery]string[] statuses, [FromQuery]int page)
+        {
+            try
+            {
+                var result = await _orderService.GetOrders(statuses, page);
+
+                if(result is null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get orders");
+                return StatusCode(500);
+            }
+        }
+
+        [HttpGet("top5-ordered")]
+        public async Task<ActionResult<IEnumerable<OrderProduct>>> GetTop5OrderedProducts()
+        {
+            try
+            {
+                var orders = await _orderService.GetInProgressOrders();
+                var result = await _orderService.GetTop5OrderedProducts(orders);
+
+                if (result is null || !result.Any())
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to get top 5 ordered products");
+                return StatusCode(500);
+            }
+        }
+    }
+}
---
+++ CE.Assessment/CE.Assessment.WebApi/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using CE.Assessment.BusinessLogic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CE.Assessment.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        private readonly IProductService _productService;
+
+        public ProductController(ILogger<ProductController> logger, IProductService productService)
+        {
+            _logger = logger;
+            _productService = productService;
+        }
+
+        [HttpPut("{merchantProductNo}")]
+        public async Task<IActionResult> UpdateProductStock(string merchantProductNo)
+        {
+            try
+            {
+                var isSuccess = await _productService.UpdateStock(merchantProductNo, 25);
+                return isSuccess? Ok(isSuccess) : StatusCode(500);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Failed to update product's stock");
+                return StatusCode(500);
+            }
+        }
+    }
+}
